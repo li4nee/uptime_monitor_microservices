dc557c45da452c861575fb9384e4313c61cc3aab
li4nee <nishantpokharel9@gmail.com>
2025-08-21 14:54:55 +0545

    feat: implement Docker setup for API Gateway, Monitor Service, and User Service with production configurations and environment variables

--- API_GATEWAY/src/globalSettings.ts
+++ API_GATEWAY/src/globalSettings.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 const GlobalSettings = {
+  NODE_ENVIRONMENT: process.env.NODE_ENV || "DEVELOPMENT",
   port: Number(4000),
   JWT_SECRET: process.env.JWT_SECRET || "miccheck1212miccheck1212",
   REDIS_URL: process.env.REDIS_URL || "redis://redis:6379",
